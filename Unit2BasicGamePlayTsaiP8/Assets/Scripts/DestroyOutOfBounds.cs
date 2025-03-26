360a7178b6620cd1e20fce0601fac09f8601ea46
rickyouall <740502@student.dusd.net>
2025-03-26 15:29:18 -0700

    making it complete
    Today i added spawning mechnic with a lot more stuff to complete the game it it works fine

--- Unit2BasicGamePlayTsaiP8/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2BasicGamePlayTsaiP8/Assets/Scripts/DestroyOutOfBounds.cs
@@ -22,6 +22,8 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over");
+
             Destroy(gameObject);
 
         }
